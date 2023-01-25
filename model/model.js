1f09c3952819925f8984d59cba6094f06ed316fb
Aidan LeMay <aidanpathways12@gmail.com>
2023-01-24 20:54:03 -0500

    Creating Mongoose Model

---
+++ model/model.js
@@ -0,0 +1,147 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const gameInfoSchema = new mongoose.Schema({
+    gameID: {
+        required: true,
+        type: Number
+    },
+    universityID: {
+        required: true,
+        type: Number
+    },
+    homeTeam: {
+        required: true,
+        type: Number
+    },
+    awayTeam: {
+        required: true,
+        type: Number
+    },
+    winningTeam: {
+        required: true,
+        type: Number
+    },
+    gameFinished: {
+        required: false,
+        type: Boolean
+    },
+    gameTime: {
+        required: false,
+        type: Date
+    }
+}, {collection: "gameInfo"});
+
+const permissionsSchema = new mongoose.Schema({
+    permissionCode: {
+        required: true,
+        type: Number
+    },
+    permissionDescription: {
+        required: true,
+        type: String
+    }
+}, {collection: "permissions"});
+
+const teamInfoSchema = new mongoose.Schema({
+    teamID: {
+        required: true,
+        type: Number
+    },
+    universityID: {
+        required: true,
+        type: Number
+    },
+    players: {
+        userID: {
+            required: true,
+            type: Number
+        }
+    },
+    description: {
+        required: false,
+        type: String
+    },
+    logo: {
+        required: false,
+        type: Image
+    },
+    approvalStatus: {
+        required: false,
+        type: Boolean,
+        default: false
+    }
+}, {collection: "teamInfo"});
+
+const universityInfoSchema = new mongoose.Schema({
+    universityID: {
+        required: true,
+        type: Number
+    },
+    moderatorIDs: {
+        userID: {
+            required: true,
+            type: Number
+        }
+    },
+    name: {
+        required: true, 
+        type: String
+    },
+    logo: {
+        required: false,
+        type: Image
+    },
+    description: {
+        required: false,
+        type: String
+    },
+    approvalStatus: {
+        required: false,
+        type: Boolean,
+        default: false
+    }
+}, {collection: "userInfo"});
+
+const userInfoSchema = new mongoose.Schema({
+    uid: {
+        required: true,
+        type: Number
+    },
+    roleID: {
+        required: true,
+        type: Number
+    },
+    universityID: {
+        required: true,
+        type: Number
+    },
+    teamID: {
+        required: false,
+        type: Number
+    },
+    firstName: {
+        required: true,
+        type: String
+    },
+    lastName: {
+        required: true,
+        type: String
+    },
+    email: {
+        required: true,
+        type: String
+    },
+    hashedPassword: {
+        required: true,
+        type: String
+    }
+}, {collection: "userInfo"});
+
+const GameInfo = mongoose.model('gameInfo', gameInfoSchema);
+const Permissions = mongoose.model('permissions', permissionsSchema);
+const TeamInfo = mongoose.model('teamInfo', teamInfoSchema);
+const UniversityInfo = mongoose.model('universityInfo', universityInfoSchema);
+const UserInfo = mongoose.model('userInfo', userInfoSchema);
+
+module.exports = {GameInfo, Permissions, TeamInfo, UniversityInfo, UserInfo};
