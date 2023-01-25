38224f98be2bc40b1de6f91e0d25755fba9df6cc
Aidan LeMay <aidanpathways12@gmail.com>
2023-01-24 19:38:46 -0500

    Building starter app, defining basic routes and standards

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+let bodyParser = require('body-parser');
+const CookieParser = require('cookie-parser');
+const port = 5000;
+
+// Middleware
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(CookieParser());
+
+// DB Scripts
+mongoose.connect(mongoString);
+const db = mongoose.connection;
+mongoose.set('strictQuery', false);
+db.on('error', (error) => {
+    console.log(error)
+})
+
+db.once('connected', () => {
+    console.log('Database Connected');
+})
+
+// Route Imports
+const defaultRoute = require('./routes/default.js');
+
+// Route Definitions
+app.use('/', defaultRoute);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port} 🙃`);
+});
