f23afba2d205c3a3bb54c7c2dcb080e4f93bddb2
Aidan LeMay <aidanpathways12@gmail.com>
2023-03-02 16:37:03 -0500

    Adding getAll and byID routes.... test em first

--- routes/game/gameRoutes.js
+++ routes/game/gameRoutes.js
@@ -5,7 +5,7 @@ require('dotenv').config(); //initialize dotenv
 let ObjectId = require("bson-objectid");
 
 // Get all game information by id
-router.get('/', async (req, res) => {
+router.get('/byID', async (req, res) => {
     // Error Checking
     if (req.body && req.body.id) {
         const game = await GameInfo.findOne({"_id": ObjectId(req.body.id)});
@@ -18,6 +18,17 @@ router.get('/', async (req, res) => {
     }
     else {
         res.status(400).json({'error': 'Request must contain university ID'});
+    }
+});
+
+// Get all game information
+router.get('/all', async (req, res) => {
+    const game = await GameInfo.find({});
+    if (game === null) {
+        res.status(400).json({'error': 'No Data Found'});
+    }
+    else {
+        res.status(200).json(game);
     }
 });
 
