8709a25cb4b4c7c00634f1bfc76ccae8b49cb3fb
Aaron Putterman <abp6318@rit.edu>
2023-03-13 14:11:45 -0400

    WH-89: More documentation for functions

--- routes/auth/validation.js
+++ routes/auth/validation.js
@@ -2,7 +2,7 @@
 // Validate that user input (or other arguments) passed in meet our criteria
 // Made with the intention of validating form input, request bodies, etc.
 
-// Validates password follows the following rules:
+// Validates password; follows the following rules:
 //      1. >= 8 characters
 //      2. alphanumeric
 //      3. require special characters (at least 1) -- 
@@ -16,6 +16,12 @@ function validatePassword(password) {
     }
 }
 
+// Validates emails; follows the following rules:
+//      1. begins with 1 or more characters [a-zA-Z0-9_.-]
+//      2. followed by an 'at' symbol (@)
+//      3. followed by 1 or more characters [a-zA-Z0-9.-] (underscores can't be used in domains)
+//      4. followed by a period (.)
+//      5. followed by 2 or more characters [a-zA-Z]
 function validateEmail(email) {
     return !!(email.match(/^(?:[a-zA-Z\d][a-zA-Z\d_-]{5,10}|[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})$/));
 }
