326c7cecb6e753a17ae12dac08bdbe6eb043216e
Aaron Putterman <abp6318@rit.edu>
2023-03-13 12:36:30 -0400

    WH-89: Started outlining functions

--- routes/auth/validation.js
+++ routes/auth/validation.js
@@ -1,3 +1,15 @@
 
 // Validate that user input (or other arguments) passed in meet our criteria
 // Made with the intention of validating form input, request bodies, etc.
+
+// Validates password follows the following rules:
+//      1. >= 8 characters
+//      2. alphanumeric
+//      3. require special characters (at least 1) -- 
+//          3a. allowed -> ~$`!@#%^&*()_-+={[}]|\:;"'<,>.?/
+//          3b. not allowed -> any whitespace
+function validatePassword(inputPassword) {
+
+};
+
+module.exports = validatePassword;
