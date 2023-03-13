83158def61ad2a298b1893920b2116c65e841817
Aaron Putterman <abp6318@rit.edu>
2023-03-13 12:07:55 -0400

    WH-89: Validation and Sanitization

---
+++ routes/auth/sanitization.js
@@ -0,0 +1,5 @@
+
+// Sanitize user input (or other arguments) passed in to:
+//      1. remove unwanted characters
+//      2. reformat value
+//      3. protect against injections
---
+++ routes/auth/validation.js
@@ -0,0 +1,3 @@
+
+// Validate that user input (or other arguments) passed in meet our criteria
+// Made with the intention of validating form input, request bodies, etc.
