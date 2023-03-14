73f66aa8ffb19964b6da18f0057c5c17df84656b
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2023-03-14 12:08:34 -0400

    added updated games and teams test - still buggy

--- __tests__/api/game_spec.js
+++ __tests__/api/game_spec.js
@@ -1,27 +1,57 @@
 const frisby = require('frisby');
+const Joi = frisby.Joi;
 
-// GET / 200 
-it('GET /games/ - expecting 200',
+//TODO: secure endpoints 
+
+//PUBLIC ENDPOINTS
+it('GET /gamePub/all - expecting 200',
+    function() {
+        return frisby.get(`http://localhost:3001/gamePub/all`)
+        .expect('status', 200)
+        .expect('jsonTypes','[*]', { 
+            "_id": Joi.string().required(),
+            "universityID": Joi.string().required(),
+            "homeTeam": Joi.string().required(),
+            "awayTeam": Joi.string().required(),
+            "winningTeam": Joi.string().required(),
+            "gameFinished": Joi.boolean().required(),
+            "gameTime": Joi.date().iso().required()
+        }); 
+});
+
+//how do i test 400 for get all games???
+
+
+it('POST /gamePub/byID - expecting 200',
  function () {
-    return frisby.get(`http://localhost:3001/games/`, {
+    return frisby.post(`http://localhost:3001/gamePub/byID`, {
         id: '63cd9f3f50a719089bd1feb6' 
 
     }) 
-    .expect('status', 200); 
+    .expect('status', 200)
+    .expect('jsonTypes','', {
+        "_id": Joi.string().required(),
+        "universityID": Joi.string().required(),
+        "homeTeam": Joi.string().required(),
+        "awayTeam": Joi.string().required(),
+        "winningTeam": Joi.string().required(),
+        "gameFinished": Joi.boolean().required(),
+        "gameTime": Joi.date().iso().required()
+    }); 
 });
 
-// GET / 400 - incorrect id
-it('GET /games/ - expecting 400',
+it('POST /gamePub/byID - expecting 400',
  function () {
-    return frisby.get(`http://localhost:3001/games/`, {
+    return frisby.post(`http://localhost:3001/gamePub/byID`, {
         id: 1 
     }) 
-    .expect('status', 400); //getting the wrong error message
+    .expect('status', 400)
+    .expect('bodyContains', 'No Data Found'); //getting the wrong error message
 });
 
-// GET / 400 - missing id
-it('GET /games/ - expecting 400',
+it('POST /gamePub/byID - expecting 400',
  function () {
-    return frisby.get(`http://localhost:3001/games/`,{}) 
-    .expect('status', 400); 
+    return frisby.post(`http://localhost:3001/gamePub/byID`,{}) 
+    .expect('status', 400)
+    .expect('bodyContains', 'Request must contain university ID'); 
 });
--- __tests__/api/team_spec.js
+++ __tests__/api/team_spec.js
@@ -1,24 +1,76 @@
 const { response } = require('express');
 const frisby = require('frisby');
 
-it('GET /teams/all - expecting 200', function () {
-    return frisby.get(`http://localhost:3001/teams/all`,) //add auth
-        .expect('status', 200); 
+//PUBLIC ENDPOINTS
+it('GET /teamPub/all - expecting 200', function () {
+    return frisby.get(`http://localhost:3001/teamPub/all`,)
+        .expect('status', 200);
+        // .expect('jsonTypes', '[*]', {
+        //     "_id": Joi.string().required(),
+        //     "universityID": Joi.string().required(),
+        //     "players": Joi.array().required(), //add in more details once arrays are finalized 
+        //     "description": Joi.string().required(),
+        //     "approvalStatus": Joi.boolean().required()
+        // }); 
 });
 
 // GET /teams/all 400 result needs test
 // on this note: how??? there are always gonna be teams 
 // need to discuss if this is a necessary test 
 
-// GET /teams/byID 200 response
-// it('GET /teams/byID - expecting 200', function () {
-//     return frisby.get(`http://localhost:3001/teams/byID`, {
-//         "id": "63cda0f91bc46e1982e593be"
-//     })
-//         .expect('status', 200); 
-// });
+it('POST /teamPub/byID - expecting 200', function () {
+    return frisby.post(`http://localhost:3001/teamPub/byID`, {
+        "_id": "63cda0f91bc46e1982e593be"
+    })
+    .expect('status', 200);
+    // .expect('jsonTypes', '', {
+    //     "_id": Joi.string().required(),
+    //     "universityID": Joi.string().required(),
+    //     "players": Joi.array().required(), //add in more details once arrays are finalized 
+    //     "description": Joi.string().required(),
+    //     "approvalStatus": Joi.boolean().required()
+    // });
+});
 
-// GET /teams/byID 400 response - bad ID
+it('POST /teamPub/byID - expecting 400 bad id', function () {
+    return frisby.post(`http://localhost:3001/teamPub/byID`, {
+        "_id": "63cda0f91bc46e1982e593fb"
+    })
+    .expect('status', 400)
+    .expect('bodyContains', 'No Data Found'); 
+});
 
-// GET /teams/byID 400 response - not req.body 
+it('POST /teamPub/byID - expecting 400 no body', function () {
+    return frisby.post(`http://localhost:3001/teamPub/byID`, {})
+    .expect('status', 400)
+    .expect('bodyContains', 'Request must contain university ID');  
+});
+
+it('POST /teamPub/byUniID - expecting 200', function () {
+    return frisby.post(`http://localhost:3001/teamPub/byUniID`, {
+        "universityID": 2760
+    })
+    .expect('status', 200);
+    // .expect('jsonTypes', '', {
+    //     "_id": Joi.string().required(),
+    //     "universityID": Joi.string().required(),
+    //     "players": Joi.array().required(), //add in more details once arrays are finalized 
+    //     "description": Joi.string().required(),
+    //     "approvalStatus": Joi.boolean().required()
+    // });
+});
+
+it('POST /teamPub/byUniID - expecting 400 bad id', function () {
+    return frisby.post(`http://localhost:3001/teamPub/byUniID`, {
+        "universityID": 1111 //keeps giving a 200????
+    })
+    .expect('status', 400)
+    .expect('bodyContains', 'No Data Found'); 
+});
+
+it('POST /teamPub/byUniID - expecting 400 no body', function () {
+    return frisby.post(`http://localhost:3001/teamPub/byUniID`, {})
+    .expect('status', 400)
+    .expect('bodyContains', 'Request must contain university ID'); 
+});
 
