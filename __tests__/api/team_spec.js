a14c06cbed44ebf511a947b09af7c38c6905be11
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2023-03-17 11:42:05 -0400

    slight changes to teams, and added university tests

--- __tests__/api/team_spec.js
+++ __tests__/api/team_spec.js
@@ -1,17 +1,17 @@
-const { response } = require('express');
 const frisby = require('frisby');
+const Joi = frisby.Joi;
 
 //PUBLIC ENDPOINTS
 it('GET /teamPub/all - expecting 200', function () {
     return frisby.get(`http://localhost:3001/teamPub/all`,)
-        .expect('status', 200);
-        // .expect('jsonTypes', '[*]', {
-        //     "_id": Joi.string().required(),
-        //     "universityID": Joi.string().required(),
-        //     "players": Joi.array().required(), //add in more details once arrays are finalized 
-        //     "description": Joi.string().required(),
-        //     "approvalStatus": Joi.boolean().required()
-        // }); 
+        .expect('status', 200)
+        .expect('jsonTypes', '[*]', {
+            "_id": Joi.string().required(),
+            "universityID": Joi.string().required(),
+            "players": Joi.array().required(), //add in more details once arrays are finalized 
+            "description": Joi.string().required(),
+            "approvalStatus": Joi.boolean().required()
+        }); 
 });
 
 // GET /teams/all 400 result needs test
@@ -22,14 +22,14 @@ it('POST /teamPub/byID - expecting 200', function () {
     return frisby.post(`http://localhost:3001/teamPub/byID`, {
         "_id": "63cda0f91bc46e1982e593be"
     })
-    .expect('status', 200);
-    // .expect('jsonTypes', '', {
-    //     "_id": Joi.string().required(),
-    //     "universityID": Joi.string().required(),
-    //     "players": Joi.array().required(), //add in more details once arrays are finalized 
-    //     "description": Joi.string().required(),
-    //     "approvalStatus": Joi.boolean().required()
-    // });
+    .expect('status', 200)
+    .expect('jsonTypes', '', {
+        "_id": Joi.string().required(),
+        "universityID": Joi.string().required(),
+        "players": Joi.array().required(), //add in more details once arrays are finalized 
+        "description": Joi.string().required(),
+        "approvalStatus": Joi.boolean().required()
+    });
 });
 
 it('POST /teamPub/byID - expecting 400 bad id', function () {
--- __tests__/api/university_spec.js
+++ __tests__/api/university_spec.js
@@ -1,46 +1,47 @@
 const frisby = require('frisby');
+const Joi = frisby.Joi;
 
 //GET /all 200
 it('GET /universities/all - expecting 200',
  function () {
-    return frisby.get(`http://localhost:3001/universities/all`) 
+    return frisby.get(`http://localhost:3001/universityPub/all`) 
     .expect('status', 200);
 });
 
 //GET /all 400 
 // again same question as teams 
 
-//giving an error bc a get cannot have a body - looking into solutions rn  
-//GET /byID 200
-// it('GET /universities/byID - expecting 200',
-//  function () {
-//     return frisby.fetch(`http://localhost:3001/universities/byID`, {
-//         method: 'QUERY',
-//         body: JSON.stringify({
-//             id: "63c99ff3a99866232ef88736"
-//         })
-//     }) 
-//     .expect('status', 200);
-// });
+//POST /byID 200
+it('POST /universities/byID - expecting 200',
+ function () {
+    return frisby.post(`http://localhost:3001/universityPub/byID`, {
+        "id": 2760
+    })
+    .expect('status', 200)
+    .expect('jsonTypes', '', {
+        "_id": Joi.string().required(),
+        "universityID": Joi.number().required(),
+        "name": Joi.string().required(), 
+        "description": Joi.string().required(),
+        "approvalStatus": Joi.boolean().required(),
+        "domain": Joi.string().required()
+    });
+});
 
-//GET /byID 400 - doesnt exist
-// it('GET /universities/byID - expecting 200',
-//  function () {
-//     return frisby.fetch(`http://localhost:3001/universities/byID`, {
-//         method: 'GET',
-//         body: JSON.stringify({
-//             id: "1"
-//         })
-//     }) 
-//     .expect('status',400);
-// });
+//POST /byID 400 - bad id
+it('POST /universities/byID - expecting 200',
+ function () {
+    return frisby.post(`http://localhost:3001/universityPub/byID`, {
+        "id": 0
+    })
+    .expect('status', 400)
+    .expect('bodyContains', 'Request must contain university ID');  
+});
 
-// //GET /byID 400 - missing id 
-// it('GET /universities/byID - expecting 200',
-//  function () {
-//     return frisby.fetch(`http://localhost:3001/universities/byID`, {
-//         method: 'GET',
-//         body: JSON.stringify({})
-//     })
-//     .expect('status',400);
-// });
+//POST /byID 400 - missing id
+it('POST /universities/byID - expecting 200',
+ function () {
+    return frisby.post(`http://localhost:3001/universityPub/byID`, {})
+    .expect('status', 400)
+    .expect('bodyContains', 'Request must contain university ID');  
+});
