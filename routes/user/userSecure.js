6d65d16aaa92b7dda2cfc757585fe9d3c4c3f0b6
Aaron Putterman <abp6318@rit.edu>
2023-03-18 13:58:03 -0400

    WH-89: Updated /put methods

--- routes/user/userSecure.js
+++ routes/user/userSecure.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { UserInfo, Permissions } = require('../../model/model');
 require('dotenv').config(); //initialize dotenv
 let ObjectId = require("bson-objectid");
+const { validateNonNullStringHashID, validateIsBoolean } = require('../auth/validation');
 
 // Get all non-admin users that have allowed marketable emails
 router.get('/getMarketable', async (req, res) => {
@@ -17,18 +18,34 @@ router.get('/getMarketable', async (req, res) => {
 });
 
 router.put('/updateMarketingPreferences', async (req, res) => {
-    // check and see if the user requesting is an admin, if so check for the included id or uid.. otherwise take it right out of the token
-    if (req.user.roleID == 14139 || req.user.roleID == 21149) { // uni admin or company admin
-        UserInfo.updateOne({uid: req.body.uid}, {canMarket: req.body.canMarket}, function(err, doc) {
-            if (err) return res.status(500).json({error: err});
-            return res.status(200).json({doc});
-        });
+
+    const { uid, canMarket } = req.body;
+    if (uid && canMarket) {
+        if (!validateNonNullStringHashID(uid)) {
+            return res.status(403).json({"error": "Invalid `uid` input"});
+        }
+        if (!validateIsBoolean(canMarket)) {
+            return res.status(403).json({"error": "Invalid `canMarket` input"});
+        }
+    } else {
+        return res.status(403).json({"error": "Invalid `uid` or `canMarket` input"});
     }
-    else {
-        UserInfo.updateOne({_id: ObjectId(req.user._id)}, {canMarket: req.body.canMarket}, function(err, doc) {
-            if (err) return res.status(500).json({error: err});
-            return res.status(200).json({doc});
-        });
+
+    // check and see if the user requesting is an admin, if so check for the included id or uid.. otherwise take it right out of the token
+    if (req.user) {
+        if (req.user.roleID == 14139 || req.user.roleID == 21149) { // uni admin or company admin
+            UserInfo.updateOne({uid: req.body.uid}, {canMarket: req.body.canMarket}, function(err, doc) {
+                if (err) return res.status(500).json({error: err});
+                return res.status(200).json({doc});
+            });
+        } else {
+            UserInfo.updateOne({_id: ObjectId(req.user._id)}, {canMarket: req.body.canMarket}, function(err, doc) {
+                if (err) return res.status(500).json({error: err});
+                return res.status(200).json({doc});
+            });
+        }
+    } else {
+        return res.status(403).json({"error": "Invalid `user` input"});
     }
 });
 
@@ -38,6 +55,11 @@ router.put('/permission', async (req, res) => {
     if (req.user.roleID == 14139 || req.user.roleID == 21149) {
 
         if (req.body.uid) {
+
+            if (!validateNonNullStringHashID(req.body.uid)) {
+                return res.status(403).json({"error": "Invalid `uid` input"});
+            }
+
             const updUser = await UserInfo.findOne({_id: ObjectId(req.body.uid)});
 
             if (updUser) {
@@ -54,8 +76,9 @@ router.put('/permission', async (req, res) => {
                     }
                 });
             }
-        }
-        
+        } 
+    } else {
+        return res.status(403).json({"error": "Invalid `role`"});
     }
 });
 
