78a09a7e7ba56670dbc25aaa046320825d4c6694
Aidan LeMay <ahl4753@rit.edu>
2023-04-04 13:51:45 -0400

    Updating team and university approval and docs, fixing team CRUD endpoints

---
+++ routes/admin/adminSecure.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+const { UserInfo, TeamInfo, UniversityInfo } = require('../../model/model');
+require('dotenv').config(); //initialize dotenv
+const ObjectId = require("bson-objectid");
+const { validateNonNullStringHashID } = require('../auth/validation');
+
+// Get Unverified Teams
+router.get('/teams', async (req, res) => {
+    if (req.user.roleID == 14139 || req.user.roleID == 21149) {
+        const unverifiedTeams = await TeamInfo.find({approvalStatus: false});
+
+        if (unverifiedTeams) {
+            res.status(200).json(unverifiedTeams);
+        }
+        else {
+            return res.status(400).json({'error': 'No Data Found'});
+        }
+    }
+    else {
+        res.status(401).json({'error': "you are not authorized to complete this action"});
+    }
+});
+
+// Verify Team By ID
+router.put('/teams', async (req, res) => {
+    if (req.user.roleID == 14139 || req.user.roleID == 21149 && req.body._id.length > 0) {
+        TeamInfo.updateOne({_id: req.body._id}, {$set: {approvalStatus: true}}, function (err, result) {
+            if (err !== null) {
+                res.status(500).json(err);
+            }
+            else {
+                res.status(200).json({result});
+            }
+        });
+    }
+    else {
+        res.status(401).json({'error': "you are not authorized to complete this action"});
+    }
+});
+
+// Get Unverified Universities
+router.get('/universities', async (req, res) => {
+    if (req.user.roleID == 14139 || req.user.roleID == 21149) {
+        const unverifiedTeams = await UniversityInfo.find({approvalStatus: false});
+
+        if (unverifiedTeams) {
+            res.status(200).json(unverifiedTeams);
+        }
+        else {
+            return res.status(400).json({'error': 'No Data Found'});
+        }
+    }
+    else {
+        res.status(401).json({'error': "you are not authorized to complete this action"});
+    }
+});
+
+// Verify University By ID
+router.put('/universities', async (req, res) => {
+    if (req.user.roleID == 14139 || req.user.roleID == 21149 && req.body._id.length > 0) {
+        UniversityInfo.updateOne({_id: req.body._id}, {$set: {approvalStatus: true}}, function (err, result) {
+            if (err !== null) {
+                res.status(500).json(err);
+            }
+            else {
+                res.status(200).json({result});
+            }
+        });
+    }
+    else {
+        res.status(401).json({'error': "you are not authorized to complete this action"});
+    }
+});
+
+module.exports = router;
