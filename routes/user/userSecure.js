7fbc3a8f02b2ad734b6c7c742586da654879096c
a-a-gordon <agordonpolarbear@gmail.com>
2023-04-05 10:03:33 -0400

    admin add new user

--- routes/user/userSecure.js
+++ routes/user/userSecure.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { UserInfo, Permissions } = require('../../model/model');
+const { UserInfo, UniversityInfo, Permissions } = require('../../model/model');
 require('dotenv').config(); //initialize dotenv
+const bcrypt = require('bcrypt');
 let ObjectId = require("bson-objectid");
+const { validateEmail, validateName, validateNonNullNumberID, validatePassword } = require('../auth/validation');
 
 // Get all non-admin users that have allowed marketable emails
 router.get('/getMarketable', async (req, res) => {
@@ -83,6 +85,85 @@ router.put('/permission', async (req, res) => {
                 }
             }
             
+        }
+    }
+    catch (error) {
+        res.status(500).json({"error": error});
+    }
+});
+
+// Create new user
+router.post('/', async (req, res) => {
+    try {
+        if (req.body && req.body.firstName && req.body.lastName && req.body.roleID && req.body.email && req.body.password) {
+            if (req.user.roleID == 14139 || req.user.roleID == 21149) {
+                console.log(req.body);
+                const { firstName, lastName, roleID, email, password } = req.body;
+
+                if (!validateName(firstName)) {
+                    return res.status(403).json({ 'error': '`first name` Provided Invalid' });
+                }
+
+                if (!validateName(lastName)) {
+                    return res.status(403).json({ 'error': '`last name` Provided Invalid' });
+                }
+
+                if (!validateNonNullNumberID(roleID)) {
+                    return res.status(403).json({ 'error': '`roleID` Provided Invalid' });
+                }
+
+                if (!validateEmail(email)) {
+                    return res.status(403).json({ 'error': '`email` Provided Invalid' });
+                }
+
+                if (!validatePassword(password)) {
+                    return res.status(403).json({ 'error': '`password` Provided Invalid' });
+                }
+                
+                //Check to see if user exists (must have unique email)
+                const existing = await UserInfo.findOne({email: email});
+
+                console.log("Successful attempt to find matching user");
+
+                if (existing) {
+                    res.status(400).json({'error': 'Email Provided already exists!'});
+                } else {
+                    console.log("EMail is unique");
+                    // email needs to end in an approved domain
+                    const emailDomain = email.split('@')[1];
+                    const domain = await UniversityInfo.findOne({ 'domain': emailDomain }, {});
+                    if (domain) {
+                        const { universityID } = domain;
+                        console.log(universityID);
+
+                        //Encrypt user password
+                        let hashedPassword = await bcrypt.hash(password, 10);
+
+                        const data = new UserInfo({
+                            roleID,
+                            universityID,
+                            firstName,
+                            lastName,
+                            email,
+                            canMarket: false,
+                            hashedPassword
+                        });
+    
+                        const inserted = await data.save();
+                        const newUser = await UserInfo.findOne({_id: inserted._id});
+                        res.status(200).json(newUser);
+                    } else {
+                        res.status(400).json({'error': 'Email Provided does not have a university domain registered in the system! Contact your university for more information.'});
+                    }
+                }
+            }
+            else {
+                res.status(401).json({'error': "you are not authorized to complete this action"});
+            }
+        }
+        else {
+            console.log("CRAP");
+            res.status(500).json({'error': "missing inputs"});
         }
     }
     catch (error) {
