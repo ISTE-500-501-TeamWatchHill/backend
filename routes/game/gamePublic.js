d7049f216ef8fc7ceb7f23fecc37566c05443c91
vsoler27 <60946932+vsoler27@users.noreply.github.com>
2023-04-15 18:53:20 -0400

    added in new endpoint

--- routes/game/gamePublic.js
+++ routes/game/gamePublic.js
@@ -29,6 +29,83 @@ router.post('/byID', async (req, res) => {
     }
 });
 
+router.post('/byTeamID', async (req, res) => {
+    try {
+        if (req.body && req.body.id) {
+            if (!validateNonNullStringHashID(req.body.id)){
+                return res.status(403).json({'error': 'Game ID provided invalid'});
+            }
+            const games = await GameInfo.aggregate([
+                {$match: { $or: [
+                    {"homeTeam": ObjectId(req.body.id)},
+                    {"awayTeam": ObjectId(req.body.id)}
+                ]}},
+                {
+                    $lookup: {
+                        from: "teamInfo",
+                        localField: "homeTeam",
+                        foreignField: "_id",
+                        as: "homeTeamInfo"
+                    }
+                },
+                {
+                    $lookup: {
+                        from: "teamInfo",
+                        localField: "awayTeam",
+                        foreignField: "_id",
+                        as: "awayTeamInfo"
+                    }
+                },
+                {
+                    $lookup: {
+                        from: "universityInfo",
+                        localField: "universityID",
+                        foreignField: "universityID",
+                        as: "locationInfo"
+                    }
+                },
+                {
+                    $project:
+                    {
+                        gameID: 1,
+                        universityID: 1,
+                        homeTeam: 1,
+                        awayTeam: 1,
+                        winningTeam: 1,
+                        gameFinished: 1,
+                        gameTime: 1,
+                        homeTeamInfo: {
+                            description: 1,
+                            logo: 1,
+                            universityID: 1,
+                        },
+                        awayTeamInfo: {
+                            description: 1,
+                            logo: 1,
+                            universityID: 1,
+                        },
+                        locationInfo: {
+                            name: 1,
+                        }
+                    }
+                }
+            ]);
+            if (games === null) {
+                return res.status(400).json({'error': 'No Data Found'});
+            }
+            else {
+                return res.status(200).json(games);
+            }
+        }
+        else {
+            return res.status(400).json({'error': 'Request must contain game ID'});
+        }
+    }
+    catch (error) {
+        res.status(500).json({"error": error});
+    }
+});
+
 // Get all game information
 router.get('/all', async (req, res) => {
     try {
