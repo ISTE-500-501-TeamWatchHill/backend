5eb51fb89c472450aae77170e473de139b8dce91
Aidan LeMay <ahl4753@rit.edu>
2023-04-17 12:49:32 -0400

    Updating parameter checking

--- routes/admin/adminSecure.js
+++ routes/admin/adminSecure.js
@@ -25,7 +25,7 @@ router.get('/teams', async (req, res) => {
 // Verify Team By ID
 router.put('/teams', async (req, res) => {
     try {
-        if (req.user.roleID == 14139 || req.user.roleID == 21149 && req.body._id.length > 0) {
+        if (req.user.roleID == 14139 || req.user.roleID == 21149 && req.body._id.length > 0 && req.body._id.length == 24) {
             TeamInfo.updateOne({_id: req.body._id}, {$set: {approvalStatus: true}}, function (err, result) {
                 if (err !== null) {
                     res.status(500).json(err);
@@ -69,7 +69,7 @@ router.get('/universities', async (req, res) => {
 // Verify University By ID
 router.put('/universities', async (req, res) => {
     try {
-        if (req.user.roleID == 14139 || req.user.roleID == 21149 && req.body._id.length > 0) {
+        if (req.user.roleID == 14139 || req.user.roleID == 21149 && req.body._id.length > 0 && req.body._id.length == 24) {
             UniversityInfo.updateOne({_id: req.body._id}, {$set: {approvalStatus: true}}, function (err, result) {
                 if (err !== null) {
                     res.status(500).json(err);
