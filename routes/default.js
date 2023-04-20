38373c80bd2cb3f50d88e9767b6a3868e8fe9671
Aaron Putterman <abp6318@rit.edu>
2023-04-20 19:52:09 -0400

    Made some changes for Natalia

--- routes/default.js
+++ routes/default.js
@@ -5,4 +5,10 @@ router.get('/', async (req, res) => {
     res.send("App Working!");
 });
 
+router.get ('/health', (_, res) => {
+    res.status(200).send({
+        uptime: Number(process.uptime().toFixed()),
+    });
+});
+
 module.exports = router;
